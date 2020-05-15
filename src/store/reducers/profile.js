3fc54290953942b768ba5c2e2d442ca3009a14f5
jaissonssantos <jaissonssantos@gmail.com>
2020-05-14 19:18:05 -0500

    add screenshots

--- src/store/reducers/profile.js
+++ src/store/reducers/profile.js
@@ -4,7 +4,7 @@ const INITIAL_STATE = {
     username: "devjs",
     bio: "A developer that is an enthusiast of new technologies and a JavaScript fan. Currently working Angular, Svelte, ReactJS, React Native, Node.js and more :)",
     picture: "https://avatars0.githubusercontent.com/u/12276656?s=460&u=a6a8a98532fa8ad0469dd0f4e3d5142a10e082d2&v=4",
-    coverPicture: "",
+    coverPicture: "https://res.cloudinary.com/ktech-cdn/image/upload/v1589501094/0_evywsz.jpg",
     city: "Rio Branco",
     url: "ktech.mobi",
     joined: new Date('2010-01-12'),
